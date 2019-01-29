0d0a89f8195996d8d5ad5770bb96996bfaa71c05
Brian D. Conversi <bdconversi@yahoo.com>
2019-01-29 14:05:16 -0600

    initial commit

---
+++ Whose-Turn-Is-It-Anyway/Whose-Turn-Is-It-Anyway/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Whose_Turn_Is_It_Anyway.MvcApplication" Language="C#" %>
---
+++ Whose-Turn-Is-It-Anyway/Whose-Turn-Is-It-Anyway/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Whose_Turn_Is_It_Anyway.Startup))]
+namespace Whose_Turn_Is_It_Anyway
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
